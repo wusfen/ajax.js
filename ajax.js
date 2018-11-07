613a0fc067226d462673e6887392c527d1622c4a
wushufen <wushufen@sunlands.com>
2018-11-07 11:05:10 +0800

    xx data.js

--- ajax.js
+++ ajax.js
@@ -1,171 +1,213 @@
-/*! @preserve https://github.com/wusfen/data.js */
-!(function () {
+/**
+ * https://github.com/wusfen/ajax.js
+ * wushufen 20171228~20180723
+ */
+!(function (window) {
+  if (!window.XMLHttpRequest) {
+    window.XMLHttpRequest = function () {
+      return new ActiveXObject('Microsoft.XMLHTTP')
+    }
+  }
+
+  var noop = function () { }
 
   var typeOf = function (obj) {
     return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
   }
 
-  var random = {
-    id: function (base, step, target) {
-      // todo target
-      base = base || 1
-      step = step || 1
-      var i = this.i = this.i || 0
-      var value = base + step * i
-      this.i += 1
-      return value
-    },
-    number: function (min, max, fixed) {
-      min = min || 0
-      max = max || 100
-      fixed = fixed || 0
-      var diff = max - min
-      var value = Math.random() * diff + min
-      value = +value.toFixed(fixed)
-      return value
-    },
-    boolean: function (rate) {
-      rate = rate || .5
-      return Math.random() < rate ? true : false
-    },
-    date: function (min, max) {
-      min = min || +new Date - 1000 * 60 * 60 * 24 * 365
-      max = max || +new Date + 1000 * 60 * 60 * 24 * 365
-      var value = random.number(min, max)
-      return new Date(value)
-    },
-    string: function (min, max, space) {
-      min = min || 10
-      max = max || min
-      space = space || ' '
-      var endCode = 0x007a
-      var startCode = 0x0061
-
-      var length = random.number(min, max)
-      var str = ''
-      for (var i = 0; i < length; i++) {
-        var code = random.number(endCode, startCode)
-        var char = String.fromCharCode(code)
-        // char = Math.random() < .16? space : String.fromCharCode(code)
-        str += char
+  var extend = function (obj, _obj) {
+    for (var key in _obj) {
+      var val = obj[key]
+      var _val = _obj[key]
+      if (typeOf(_val) == 'object') {
+        obj[key] = extend(val ? val : {}, _val)
+      } else {
+        obj[key] = _obj[key]
       }
-      return str
-    },
-    zh: function (min, max) {
-      min = min || 25
-      max = max || min
-      var chars = '我人有的和主产不为这工要在地一上是中国经以发了民同'
-      var length = random.number(min, max)
-      var str = ''
-      for (var i = 0; i < length; i++) {
-        var index = random.number(0, chars.length - 1)
-        var char = chars.charAt(index)
-        str += char
-      }
-      return str
-    },
-    repeat: function (s, min, max) {
-      s = xquo(s)
-      var length = random.number(min, max)
-      var str = ''
-      for (var i = 0; i < length; i++) {
-        str += s
-      }
-      return str
-    },
-    url: function (min, max) {
-      min = min || 5
-      max = max || 20
-      return 'http://' + random.string(3, 7) + '.com/' + random.string(min, max)
-    },
-    email: function (min, max) {
-      min = min || 5
-      max = max || 10
-      return random.string(min, max) + '@' + random.string(3, 7) + '.com'
-    },
-    name: function () {
-      var n1 = '赵钱孙李周吴郑王'.split('')[random.number(0, 7)]
-      var n2 = '春夏秋冬,'.split(/,|\B/)[random.number(0, 4)]
-      var n3 = '梅兰竹菊'.split('')[random.number(0, 3)]
-      return n1 + n2 + n3
-    },
-    img: function (width, height, bg, text) {
-      width = width || 200
-      height = height || 100
-      bg = bg || '#eee'
-      text = text || width + ' x ' + height
-
-      if (typeof document == 'undefined') return
-      var canvas = this.canvas = this.canvas || document.createElement('canvas')
-      canvas.width = width
-      canvas.height = height
-      var ctx = canvas.getContext('2d')
-
-      // document.body.appendChild(canvas)
-      // window.canvas = canvas
-      // window.ctx = ctx
-
-      ctx.fillStyle = bg
-      ctx.fillRect(0, 0, width, height)
-
-      var fontSize = width < 70 ? 10 : 20
-      ctx.fillStyle = '#a9a9a9'
-      ctx.font = fontSize + 'px Arial'
-      ctx.textAlign = 'center'
-      ctx.textBaseline = 'middle'
-      ctx.fillText(text, width / 2, height / 2)
-
-      return canvas.toDataURL()
     }
+    return obj
   }
 
-  function Data(rule) {
-    if ('string' == typeOf(rule)) {
-      var m_a = rule.split('(') // method(100, 5) => ['method', '100, 5)']
-      // method
-      var method = m_a[0]
-      if (method in random) {
-        var args = (m_a[1] || '').replace(')', '')
-        // args
-        args = args.split(',')
-        // to number
-        for (var i = args.length - 1; i >= 0; i--) {
-          var arg = args[i]
-          // '"arg"' => 'arg'
-          args[i] = arg.replace(/^\s*["']|["']\s*$/g, '')
-          if (!isNaN(arg)) {
-            args[i] = Number(arg)
-          }
+  var map = function (arr, fn) {
+    var list = []
+    for (var i = 0; i < arr.length; i++) {
+      list[i] = fn(arr[i], i)
+    }
+    return list
+  }
+
+  // to x-www-form-urlencoded
+  var encodeUrl = function (value) {
+    var arr = [] // [ {keys, value}, ]
+
+    !(function loop(keys, value) {
+
+      if (typeOf(value) == 'undefined') {
+        // nothing
+      } else if (typeOf(value) == 'null') {
+        arr.push({ keys: keys, value: '' })
+
+      } else if (typeOf(value) == 'function') {
+        arr.push({ keys: keys, value: value() })
+
+      } else if (typeOf(value) == 'object' || typeOf(value) == 'array') {
+
+        for (var k in value) {
+          if (!value.hasOwnProperty(k)) continue
+
+          loop(keys.concat(k), value[k]) // [k1,k2,..]
         }
-        // ()
-        return random[method].apply(random, args)
+
+      } else {
+        arr.push({ keys: keys, value: value })
       }
-    }
-    if ('object' == typeOf(rule)) {
-      var obj = {}
-      for (var key in rule) {
-        var value = rule[key]
-        obj[key] = Data(value)
-      }
-      return obj
-    }
-    if ('array' == typeOf(rule)) {
-      var arr = []
-      var obj = rule[0]
-      var min = rule[1] || 1
-      var max = rule[2] || min
-      var length = obj ? random.number(min, max) : 0
-      for (var i = 0; i < length; i++) {
-        arr[i] = Data(obj)
-      }
-      return arr
-    }
-    return rule
+    })([], value)
+
+    // [ 'obj[key]=value', ]
+    arr = map(arr, function (item, i) {
+      var keys = item.keys
+      var value = item.value
+      var keyPath = ''
+      map(keys, function (key, i) {
+        var _key = '[' + key + ']'
+        if (i == 0) {
+          _key = key
+        }
+        var lastI = keys.length - 1
+        if (i == lastI && !isNaN(keys[lastI])) {
+          _key = '[]'
+        }
+        keyPath += _key
+      })
+      // ecKeyPath=ecValue
+      keyPath = encodeURIComponent(keyPath).replace(/%20/g, '+')
+      value = encodeURIComponent(value).replace(/%20/g, '+')
+      return keyPath + (keyPath ? '=' : '') + value
+    })
+
+    // ...&...
+    return arr.join('&')
   }
 
-  if (typeof module != 'undefined') {
-    module.exports = Data
-  } else {
-    window.Data = Data
+  var setting = {
+    base: '',
+    url: '',
+    type: 'get',
+    data: {},
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    async: true,
+    cache: false,
+    before: noop,
+    after: noop,
+    success: noop,
+    error: noop
   }
-})()
+
+  function ajax(options) {
+    var _setting = extend({}, setting)
+    options = extend(_setting, options)
+    options.type = options.type.toUpperCase()
+    ajax.options = options
+
+    // before
+    if (options.before(xhr, options)) {
+      return
+    }
+
+    // data
+    var data = options.data
+    // raw: text||application/json
+    if (typeOf(data) == 'string') {
+      options.dataStr = data
+    }
+    // form
+    else {
+      options.dataStr = encodeUrl(data)
+    }
+
+    // href
+    options.href = options.url
+    if (options.base && !options.url.match('://')) {
+      options.href = options.base + '/' + options.url
+    }
+    if (options.type == 'GET') {
+      options.href = options.href + (options.href.match(/\?/) ? '&' : '?') + options.dataStr
+    }
+
+
+    // xhr
+    var xhr = new XMLHttpRequest
+    ajax.xhr = xhr
+
+    // handle
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState != 4) return
+      // console.log(xhr.readyState, xhr.status, xhr.responseText)
+
+      // res
+      var res = xhr.responseText
+      try { res = JSON.parse(res) } catch (e) {
+        // something
+      }
+      ajax.res = res
+
+      // after
+      if (options.after(xhr, options, res)) {
+        return
+      }
+
+      // success
+      // xhr.status == 0 || 
+      var status = xhr.status
+      if (status == 0 || (status >= 200 && status < 300) || status == 304) {
+        options.success(res)
+      }
+      // error
+      else {
+        options.error(xhr)
+      }
+    }
+
+    // open
+    xhr.open(options.type, options.href, options.async)
+
+    // headers
+    for (var key in options.headers) {
+      xhr.setRequestHeader(key, options.headers[key])
+    }
+
+    // no-cache
+    if (!options.cache) {
+      xhr.setRequestHeader('If-Modified-Since', 0)
+    }
+
+    // send
+    xhr.send(options.type == 'POST' ? options.dataStr : null)
+
+    return xhr
+  }
+
+  ajax.setting = setting
+  ajax.post = function (options) {
+    options.type = 'POST'
+    return ajax(options)
+  }
+  ajax.get = function (options) {
+    options.type = 'GET'
+    return ajax(options)
+  }
+  ajax.config = function (options) {
+    return extend(setting, options)
+  }
+  ajax.setUp = ajax.config
+
+  // export
+  if (typeof module != 'undefined') {
+    module.exports = ajax
+  } else {
+    window.ajax = ajax
+  }
+})(window);
