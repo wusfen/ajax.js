78429f7f6f59e7c692460f8f081e16de0ab2214c
wushufen <wushufen@sunlands.com>
2019-06-04 15:48:02 +0800

    自动转换数据格式

--- ajax.js
+++ ajax.js
@@ -1,208 +1,98 @@
 /*! @preserve https://github.com/wusfen/ajax.js */
 !(function (window) {
-    if (!window.XMLHttpRequest) {
-        window.XMLHttpRequest = function () {
-            return new ActiveXObject("Microsoft.XMLHTTP")
-        }
+  if (!window.XMLHttpRequest) {
+    window.XMLHttpRequest = function () {
+      return new ActiveXObject("Microsoft.XMLHTTP")
+    }
+  }
+
+  function ajax(options) {
+    var xhr = new XMLHttpRequest
+
+    // res
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState != 4) return
+      var status = xhr.status
+      var res = xhr.responseText
+      try { res = JSON.parse(res) } catch (e) { }
+
+      // success
+      if (status == 0 || (status >= 200 && status < 300) || status == 304) {
+        options.success && options.success(res)
+      }
+      // error
+      else {
+        options.error && options.error(xhr)
+      }
     }
 
-    var noop = function () { }
-
-    var typeOf = function (obj) {
-        return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+    // headers
+    var headers = {
+      'X-Requested-With': 'XMLHttpRequest',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+    for (var k1 in options.headers) {
+      var key = k1
+      for(var k2 in headers) if(k1.toLowerCase() == k2.toLowerCase()) key = k2
+      headers[key] = options.headers[k1]
     }
 
-    var extend = function (obj, _obj) {
-        for (var key in _obj) {
-            var val = obj[key]
-            var _val = _obj[key]
-            if (typeOf(_val) == 'object') {
-                obj[key] = extend(val ? val : {}, _val)
-            } else {
-                obj[key] = _obj[key]
-            }
-        }
-        return obj
+    // type
+    var type = (options.type || 'get').toUpperCase()
+
+    // data
+    var data = options.data || ''
+    if (typeof data == 'object') {
+      if (headers['Content-Type'].match('form-urlencoded')) data = toUrlSearchParams(data)
+      if (headers['Content-Type'].match('json')) data = JSON.stringify(data)
     }
 
-    var map = function (arr, fn) {
-        var list = []
-        for (var i = 0; i < arr.length; i++) {
-            list[i] = fn(arr[i], i)
-        }
-        return list
+    // href
+    var href = options.href || '.'
+    if (type == 'GET' && data) {
+      href += (href.match(/\?/) ? '&' : '?') + data
     }
 
-    // to x-www-form-urlencoded
-    var encodeUrl = function (value) {
-        var arr = [] // [ {keys, value}, ]
+    console.log(type, data, href, headers)
 
-        !(function loop(keys, value) {
-
-            if (typeOf(value) == 'undefined') {
-
-            } else if (typeOf(value) == 'null') {
-                arr.push({ keys: keys, value: '' })
-
-            } else if (typeOf(value) == 'function') {
-                arr.push({ keys: keys, value: value() })
-
-            } else if (typeOf(value) == 'object' || typeOf(value) == 'array') {
-
-                for (var k in value) {
-                    if (!value.hasOwnProperty(k)) continue
-
-                    loop(keys.concat(k), value[k]) // [k1,k2,..]
-                }
-
-            } else {
-                arr.push({ keys: keys, value: value })
-            }
-        })([], value)
-
-        // [ 'obj[key]=value', ]
-        arr = map(arr, function (item, i) {
-            var keys = item.keys
-            var value = item.value
-            var keyPath = ''
-            map(keys, function (key, i) {
-                var _key = '[' + key + ']'
-                if (i == 0) {
-                    _key = key
-                }
-                var lastI = keys.length - 1
-                if (i == lastI && !isNaN(keys[lastI])) {
-                    _key = '[]'
-                }
-                keyPath += _key
-            })
-            // ecKeyPath=ecValue
-            keyPath = encodeURIComponent(keyPath).replace(/%20/g, '+')
-            value = encodeURIComponent(value).replace(/%20/g, '+')
-            return keyPath + (keyPath ? '=' : '') + value
-        })
-
-        // ...&...
-        return arr.join('&')
+    // send
+    xhr.open(type, href, true)
+    for (var key in headers) {
+      xhr.setRequestHeader(key, headers[key])
     }
+    xhr.send(type == 'POST' ? data : null)
+    return xhr
+  }
 
-    var setting = {
-        base: '',
-        url: '',
-        type: 'get',
-        data: {},
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        },
-        async: true,
-        cache: false,
-        before: noop,
-        after: noop,
-        success: noop,
-        error: noop
+  // x-www-form-urlencoded
+  function toUrlSearchParams(value) {
+    var params = ''
+
+    function loopValue(key, value) {
+      if (value && (value.constructor == Object || value instanceof Array)) {
+        for (var k in value) {
+          if (!value.hasOwnProperty(k)) continue
+          var subKey = !key ? k : '[' + k + ']' // obj[k]
+          loopValue(key + subKey, value[k])
+        }
+      } else {
+        if (value === undefined || value === null) value = ''
+        if (typeof value === 'function') value = value()
+        key = key.replace(/\[\d+?\]$/g, '[]') // obj[arr][0] => obj[arr][]
+        key = encodeURIComponent(key)
+        value = encodeURIComponent(value)
+        params += (params ? '&' : '') + key + '=' + value // &obj[k][s]=value
+      }
     }
+    loopValue('', value)
 
-    function ajax(options) {
-        var _setting = extend({}, setting)
-        options = extend(_setting, options)
-        options.type = options.type.toUpperCase()
-        ajax.options = options
+    return params.replace(/%20/g, '+') // ' ' => '+'
+  }
 
-        // before
-        if (options.before(xhr, options)) {
-            return
-        }
-
-        // data
-        var data = options.data
-        // raw: text||application/json
-        if (typeOf(data) == 'string') {
-            options.dataStr = data
-        }
-        // form
-        else {
-            options.dataStr = encodeUrl(data)
-        }
-
-        // href
-        options.href = options.url
-        if (options.base && !options.url.match('://')) {
-            options.href = options.base + '/' + options.url
-        }
-        if (options.type == 'GET') {
-            options.href = options.href + (options.href.match(/\?/) ? '&' : '?') + options.dataStr
-        }
-
-
-        // xhr
-        var xhr = new XMLHttpRequest
-        ajax.xhr = xhr
-
-        // handle
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState != 4) return
-            // console.log(xhr.readyState, xhr.status, xhr.responseText)
-
-            // res
-            var res = xhr.responseText
-            try { res = JSON.parse(res) } catch (e) { }
-            ajax.res = res
-
-            // after
-            if (options.after(xhr, options, res)) {
-                return
-            }
-
-            // success
-            // xhr.status == 0 || 
-            var status = xhr.status
-            if (status == 0 || (status >= 200 && status < 300) || status == 304) {
-                options.success(res)
-            }
-            // error
-            else {
-                options.error(xhr)
-            }
-        }
-
-        // open
-        xhr.open(options.type, options.href, options.async)
-
-        // headers
-        for (var key in options.headers) {
-            xhr.setRequestHeader(key, options.headers[key])
-        }
-
-        // no-cache
-        if (!options.cache) {
-            xhr.setRequestHeader('If-Modified-Since', 0)
-        }
-
-        // send
-        xhr.send(options.type == 'POST' ? options.dataStr : null)
-
-        return xhr
-    }
-
-    ajax.setting = setting
-    ajax.post = function (options) {
-        options.type = 'POST'
-        return ajax(options)
-    }
-    ajax.get = function (options) {
-        options.type = 'GET'
-        return ajax(options)
-    }
-    ajax.config = function (options) {
-        return extend(setting, options)
-    }
-    ajax.setUp = ajax.config
-
-    // export
-    if (typeof module != 'undefined') {
-        module.exports = ajax
-    } else {
-        window.ajax = ajax
-    }
+  // export
+  if (typeof module != 'undefined') {
+    module.exports = ajax
+  } else {
+    window.ajax = ajax
+  }
 })(window);
